6737f7a7e6405529071b3a427664a3fb68480f9d
Fan Gao <fancrazy88@gmail.com>
2025-11-17 22:50:54 -0700

    code update

---
+++ src/components/SecondComp.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { inject } from 'vue';
+const parentValue = inject('parentValue');
+</script>
+
+<template>
+  <div>
+    <h2>Injected Value: {{ parentValue }}</h2>
+  </div>
+</template>
